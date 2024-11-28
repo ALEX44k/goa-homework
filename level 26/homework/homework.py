eabffebae08201b493a031da15ffa2e22ed23b42
ALEX44k <alexprofy4232@gmail.com>
2024-11-28 19:40:58 +0300

    commit

---
+++ level 26/homework/homework.py
@@ -0,0 +1,11 @@
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return "Even"
+#     else:
+#         return "odd"
+
+
+# def multiply(a,b):
+#     return a * b
+# print(multiply(8,4))
+    
